e3c1fd5d0290a10ca925319a09f834d0904ab089
Danielle Sanches <128653080+DanySanches@users.noreply.github.com>
2024-08-16 16:28:48 -0300

    Update Ex10.java

--- Ex10.java
+++ Ex10.java
@@ -9,9 +9,9 @@ resultado.
 public class Ex10{
 	public static void main(String[] args) {
 	    int a = 6;
-        int b = 4;
-        int c = a/b;
-		System.out.println(c);
+            int b = 4;
+            int c = a/b;
+	    System.out.println(c);
 	}
 }
 
