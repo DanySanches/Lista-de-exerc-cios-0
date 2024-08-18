ac7ce24c4869113eefa292ff5daa7ac138125569
Danielle Sanches <128653080+DanySanches@users.noreply.github.com>
2024-08-18 13:44:48 -0300

    Update Ex7.java

--- Ex7.java
+++ Ex7.java
@@ -1,7 +1,5 @@
-/**Ler dois valores para as variáveis A e B, efetuar a troca dos valores de forma
-que a variável A passe a possuir o valor da variável B e 
-que a variável B passe a possuir o valor da variável A. 
-Apresentar os valores trocados.*/
+/*Ler dois valores para as variáveis A e B, efetuar a troca dos valores de forma que a variável A passe a possuir o valor da variável B e 
+que a variável B passe a possuir o valor da variável A. Apresentar os valores trocados.*/
 import java.util.Scanner;
 
 public class Ex7 {
