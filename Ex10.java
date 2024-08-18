ec2a8722d0f94970c4a84e4b5d8df957a8d4f42c
Danielle Sanches <128653080+DanySanches@users.noreply.github.com>
2024-08-18 13:46:16 -0300

    Update Ex10.java

--- Ex10.java
+++ Ex10.java
@@ -1,4 +1,4 @@
-/**10. Faça um programa com 3 variáveis do tipo inteiro (int) tal que a primeira tenha 
+/* Faça um programa com 3 variáveis do tipo inteiro (int) tal que a primeira tenha 
 o valor de 6, a segunda o valor 4 e a terceira receba o valor da divisão da
 primeira pela segunda. Exiba o valor da terceira variável. Faça uma análise do
 resultado.
